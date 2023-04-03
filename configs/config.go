5ba651b6fd482b014ce3f9d2ae3703867d7fa626
andreis3 <andrei.as3@hotmail.com>
2023-04-03 07:45:48 -0300

    structuring directory files

---
+++ configs/config.go
@@ -0,0 +1,21 @@
+package configs
+
+var cfg *conf
+
+type conf struct {
+	DBDriver      string
+	DBHost        string
+	DBPort        string
+	DBUser        string
+	DBPassword    string
+	DBName        string
+	WebServerPort string
+	JWTSecret     string
+	JWTExpireIn   int
+}
+
+func LoadConfig(path string) (*conf, error) {
+	//...
+
+	return cfg, nil
+}
