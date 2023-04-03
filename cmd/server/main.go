3b82c02525e97121ff14366837bf49f47e08f8c6
andreis3 <andrei.as3@hotmail.com>
2023-04-03 08:29:51 -0300

    finalizing configuration

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "github.com/andreis3/api-golang/configs"
 
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	println(configs.DBDriver)
 }
