51435084de1d9248bb8b08388a38518f6ca94e03
andreis3 <andrei.as3@hotmail.com>
2023-04-11 07:16:06 -0300

    create handlers archives

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 
 	"github.com/andreis3/api-golang/configs"
-	"github.com/andreis3/api-golang/internal/dto"
 	"github.com/andreis3/api-golang/internal/entity"
 	"github.com/andreis3/api-golang/internal/infra/database"
+	handlers "github.com/andreis3/api-golang/internal/infra/webserver"
 )
 
 func main() {
@@ -27,39 +26,9 @@ func main() {
 
 	productDB := database.NewProduct(db)
 
-	productHandler := NewProductHandler(productDB)
+	productHandler := handlers.NewProductHandler(productDB)
 
 	http.HandleFunc("/products", productHandler.CreateProduct)
 
 	http.ListenAndServe(":8080", nil)
-}
-
-type ProductHandler struct {
-	ProductDB database.ProductInterface
-}
-
-func NewProductHandler(db database.ProductInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var product dto.CreateProductInput
-	err := json.NewDecoder(r.Body).Decode(&product)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	p, err := entity.NewProduct(product.Name, product.Price)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	err = h.ProductDB.Create(p)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	w.WriteHeader(http.StatusCreated)
 }
