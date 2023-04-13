f35d728cc56082fff67941f18757da8fe3198d1e
andreis3 <andrei.as3@hotmail.com>
2023-04-13 08:16:28 -0300

    finalizado!!!

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,12 +11,32 @@ import (
 
 	"github.com/go-chi/chi/v5"
 
+	httpSwagger "github.com/swaggo/http-swagger"
+
 	"github.com/andreis3/api-golang/configs"
+	_ "github.com/andreis3/api-golang/docs"
 	"github.com/andreis3/api-golang/internal/entity"
 	"github.com/andreis3/api-golang/internal/infra/database"
 	handlers "github.com/andreis3/api-golang/internal/infra/webserver"
 )
 
+// @title API Golang Example
+// @version 1.0.0
+// @description This is a sample server for a store API.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name Andrei Santos
+// @contact.url http://www.swagger.io/support
+// @contact.email andrei.santos.dev@gmail.com
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /api/v1
+// @securityDefinitions.apiKey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
 	configs, err := configs.LoadConfig(".")
 	if err != nil {
@@ -54,6 +74,8 @@ func main() {
 	// users routes
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/users/generate-token", userHandler.GetJWT)
+
+	r.Get("docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8080/docs/doc.json")))
 
 	http.ListenAndServe(":8080", r)
 }
--- internal/infra/webserver/user_handlers.go
+++ internal/infra/webserver/user_handlers.go
@@ -12,6 +12,10 @@ import (
 	"github.com/andreis3/api-golang/internal/infra/database"
 )
 
+type Error struct {
+	Message string `json:"message"`
+}
+
 type UserHandler struct {
 	UserDB database.UserInterface
 }
@@ -22,6 +26,17 @@ func NewUserHandler(db database.UserInterface) *UserHandler {
 	}
 }
 
+// GetJWT godoc
+// @Summary      Get a user JWT
+// @Description  Get a user JWT
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        request   body     dto.GetJWTInput  true  "user credentials"
+// @Success      200  {object}  dto.GetJWTOutput
+// @Failure      404  {object}  Error
+// @Failure      500  {object}  Error
+// @Router       /users/generate_token [post]
 func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	jwt := r.Context().Value("jwt").(*jwtauth.JWTAuth)
 	jwtExpiresIn := r.Context().Value("jwt_expires_in").(int)
@@ -35,7 +50,9 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 
 	user, err := h.UserDB.FindByEmail(userDTO.Email)
 	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
+		w.WriteHeader(http.StatusNotFound)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
@@ -49,9 +66,7 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 		"exp": time.Now().Add(time.Second * time.Duration(jwtExpiresIn)).Unix(),
 	})
 
-	accessToken := struct {
-		AccessToken string `json:"access_token"`
-	}{
+	accessToken := dto.GetJWTOutput{
 		AccessToken: tokenString,
 	}
 
@@ -61,6 +76,16 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// Create user godoc
+// @Summary      Create user
+// @Description  Create user
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        request     body      dto.CreateUserInput  true  "user request"
+// @Success      201
+// @Failure      500         {object}  Error
+// @Router       /users [post]
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var userDTO dto.CreateUserInput
 	err := json.NewDecoder(r.Body).Decode(&userDTO)
@@ -72,12 +97,16 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	user, err := entity.NewUser(userDTO.Name, userDTO.Email, userDTO.Password)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
 	err = h.UserDB.Create(user)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		error := Error{Message: err.Error()}
+		json.NewEncoder(w).Encode(error)
 		return
 	}
 
